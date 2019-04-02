816eb6563de159c218f934cadbb3d5c4a88b1dc0
Nisheeth Lahoti <nisheeth.lahoti@gmail.com>
2019-04-03 01:18:55 +0530

    Adding python console that logs input

---
+++ bin/console_logged.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+from argparse import ArgumentParser
+import code
+import readline
+import rlcompleter
+import sys
+
+
+def logged_input(prompt):
+    inp = input(prompt)
+    logfile.write(inp + '\n')
+    return inp
+
+
+parser = ArgumentParser(description='Interactive python console that logs all its input to a file')
+parser.add_argument('logfile', help='File to log to')
+sys.path.append('.')
+logfile = open(parser.parse_args().logfile, 'w')
+console = code.InteractiveConsole()
+console.raw_input = logged_input
+readline.set_completer(rlcompleter.Completer(console.locals).complete)
+readline.parse_and_bind("tab: complete")
+banner = f'Python version {sys.version} Logged Console running on {sys.executable}'
+console.interact(banner, f'Logged output to {logfile.name}')
