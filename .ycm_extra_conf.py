17b2711c4634d7fba2dfc858a533a88b4810bb50
Nisheeth Lahoti <nisheeth.lahoti@gmail.com>
2019-04-03 13:42:37 +0530

    Removing some useless config files and changing paths of others

--- .ycm_extra_conf.py
+++
@@ -1,4 +0,0 @@
-def FlagsForFile( filename, **kwargs ):
-	return {
-		'flags': [ '-x', 'c++', '-std=gnu++1z', '-Wall',  '-Werror' ],
-	}
--- bin/rust-update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if wget -O - https://static.rust-lang.org/dist/channel-rust-nightly.toml | grep rustfmt-preview > /dev/null
-then
-	rustup update nightly
-else
-	echo "Current nightly build not good" >&2
-	exit 1
-fi
