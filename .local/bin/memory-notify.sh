eb2d99f75bb7c40c19310138da59f98617a9ff0b
Nisheeth Lahoti <nisheeth.lahoti@gmail.com>
2019-04-16 15:04:08 +0530

    Checking swap every second (run as cron on reboot)

--- .local/bin/memory-notify.sh
+++ .local/bin/memory-notify.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+# Run with cron on reboot
 export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$UID/bus"
-used_swap=$(awk '{print $4}' /proc/swaps | tail -n 1)
-free_swap=$(awk '{print $3}' /proc/swaps | tail -n 1)
+TOTAL_SWAP=$(awk '{print $3}' /proc/swaps | tail -n 1)
+HALF_SWAP=$((TOTAL_SWAP / 2))
 
-if [[ $((used_swap * 2)) -gt $free_swap ]]
-then
-	notify-send "Half of swap space filled!"
-fi
+while true
+do
+	used_swap=$(awk '{print $4}' /proc/swaps | tail -n 1)
+	if [[ $used_swap -gt $HALF_SWAP ]]
+	then
+		notify-send "Half of swap space filled!"
+		sleep 60
+	else
+		sleep 1
+	fi
+done
