1d6e1633dce5d7c02d30e48706a0f8ef7ae9d300
Nisheeth Lahoti <nisheeth.lahoti@gmail.com>
2019-05-18 19:06:23 +0530

    Letting console_logged.py take command line arguments

--- .local/bin/console_logged.py
+++ .local/bin/console_logged.py
@@ -15,9 +15,10 @@ def logged_input(prompt):
 
 parser = ArgumentParser(description='Interactive python console that logs all its input to a file')
 parser.add_argument('logfile', help='File to log to')
+parser.add_argument('args', nargs='*', help='Additional things to be passed on command line')
 sys.path.append('.')
 logfile = open(parser.parse_args().logfile, 'w')
-console = code.InteractiveConsole()
+console = code.InteractiveConsole(locals={'args': parser.parse_args().args})
 console.raw_input = logged_input
 readline.set_completer(rlcompleter.Completer(console.locals).complete)
 readline.parse_and_bind("tab: complete")
